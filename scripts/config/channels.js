950cd207951f0952ee918f22c7b346d4552ac663
u_mito <salmon.river.310@gmail.com>
2026-01-20 03:38:15 +0900

    fix: channel.js修正

--- scripts/config/channels.js
+++ scripts/config/channels.js
@@ -1,8 +1,11 @@
 /**
  * 対象チャンネル定義
+ *
  * 全スクリプト共通の単一情報源
+ * fetch_videos.js / fetch_comments.js 共通
  */
-export const CHANNELS = {
+
+const CHANNELS = {
   channelA: {
     channelId: 'UCrxtv0Zc8uQNfsY0HsAGY8g',
     channelName: '天硝路ろまん'
@@ -12,3 +15,5 @@ export const CHANNELS = {
     channelName: '華鉈イオ'
   }
 };
+
+export default CHANNELS;
